2fcfe771e80ae4a944b8a5b4186b7096c840c375
danishbukhari4599 <danishbukhari4599@gmail.com>
2024-04-04 03:05:27 +0500

    Add project files.

---
+++ Battery_Monitor/Service1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Management;
+using System.Speech.Synthesis;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace Battery_Monitor
+{
+    public partial class BatteryMonitorService : ServiceBase
+    {
+        private Timer timer;
+        private SpeechSynthesizer synthesizer;
+        private bool isBatteryFullNotified = false;
+        private bool isBatteryLowNotified = false;
+
+        public BatteryMonitorService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer();
+            timer.Interval = 60000; // Check every minute
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            synthesizer = new SpeechSynthesizer();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            CheckBatteryStatus();
+        }
+
+        private void CheckBatteryStatus()
+        {
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Battery");
+            var battery = searcher.Get().OfType<ManagementObject>().FirstOrDefault();
+
+            if (battery != null)
+            {
+                int batteryLevel = Convert.ToInt32(battery["EstimatedChargeRemaining"]);
+
+                if (batteryLevel > 90)
+                {
+                    if (!isBatteryFullNotified)
+                    {
+                        synthesizer.Speak("Battery is full, please unplug the charger.");
+                        isBatteryFullNotified = true;
+                        isBatteryLowNotified = false;
+                    }
+                }
+                else if (batteryLevel <= 10)
+                {
+                    if (!isBatteryLowNotified)
+                    {
+                        synthesizer.Speak("Battery is low, please plug in the charger.");
+                        isBatteryLowNotified = true;
+                        isBatteryFullNotified = false;
+                    }
+                }
+                else
+                {
+                    isBatteryLowNotified = false;
+                    isBatteryFullNotified = false;
+                }
+            }
+        }
+    }
+}
